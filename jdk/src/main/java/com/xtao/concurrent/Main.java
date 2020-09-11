c69dadd84ca474bb7730960f8455eacfcc688b04
diyi <diyi@wacai.com>
2020-09-11 15:36:36 +0800

    init

---
+++ jdk/src/main/java/com/xtao/concurrent/Main.java
@@ -0,0 +1,11 @@
+package com.xtao.concurrent;
+
+/**
+ * @author diyi
+ * @since 2020/9/11 3:33 下午
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("hello");
+    }
+}
