67fa51af95d8932c41a172ba908f3a196a60a6a0
diyi <diyi@wacai.com>
2020-09-11 15:41:07 +0800

    remove targe

--- jdk/src/main/java/com/xtao/concurrent/Main.java
+++ jdk/src/main/java/com/xtao/concurrent/Main.java
@@ -6,6 +6,9 @@ package com.xtao.concurrent;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("hello");
+        Thread thread = Thread.currentThread();
+        for (StackTraceElement stackTraceElement : thread.getStackTrace()) {
+            System.out.printf(stackTraceElement.toString());
+        }
     }
 }
